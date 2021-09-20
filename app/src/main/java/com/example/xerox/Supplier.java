aab26ad51f07c4fa76edde4e646eadbdf7b3ca7d
Sandali Jayawickrama <it20095062@my.sliit.lk>
2021-09-20 21:15:29 +0530

    Add registration page - sandali

--- app/src/main/java/com/example/xerox/Supplier.java
+++ app/src/main/java/com/example/xerox/Supplier.java
@@ -1,4 +1,44 @@
 package com.example.xerox;
 
 public class Supplier {
+    private String userName;
+    private String TelNo;
+    private String Email;
+    private String Pword;
+
+
+    public Supplier() {
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getTelNo() {
+        return TelNo;
+    }
+
+    public void setTelNo(String telNo) {
+        TelNo = telNo;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getPword() {
+        return Pword;
+    }
+
+    public void setPword(String pword) {
+        Pword = pword;
+    }
 }
