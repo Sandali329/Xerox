caa8e42f7a8d679ae5a4aa2fce66e9b1559dd479
Sandali Jayawickrama <it20095062@my.sliit.lk>
2021-09-21 17:59:29 +0530

    Add main and login pages -S

--- app/src/main/java/com/example/xerox/MainActivity.java
+++ app/src/main/java/com/example/xerox/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.xerox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button signIn;
+    private Button signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signIn = findViewById(R.id.btn_signin);
+        signUp = findViewById(R.id.btn_signup);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(i);
+            }
+        });
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(in);
+            }
+        });
+
     }
 }
