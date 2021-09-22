573ca7418bb3210320cb71b280840034d5f9bede
Sandali Jayawickrama <it20095062@my.sliit.lk>
2021-09-22 09:53:33 +0530

    Add sup1 - S

---
+++ app/src/main/java/com/example/xerox/SupItemAddActivity.java
@@ -0,0 +1,14 @@
+package com.example.xerox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SupItemAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sup_item_add);
+    }
+}
--- app/src/main/java/com/example/xerox/SupplierActivity1.java
+++ app/src/main/java/com/example/xerox/SupplierActivity1.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class SupplierActivity1 extends AppCompatActivity {
     EditText et_supName;
     String name;
+
+    Button fruits, veg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,4 +25,19 @@ public class SupplierActivity1 extends AppCompatActivity {
         name = start.getStringExtra("name");
         et_supName.setText(name);
     }
+
+    public void gotoFruitsPage(){
+
+    }
+
+    public void FruitsPage(View view) {
+        Intent intent1 = new Intent(this,VIEWFRUIT.class);
+        startActivity(intent1);
+    }
+
+    public void VegPage(View view) {
+        Intent intent1 = new Intent(this,VIEWFRUIT.class);
+        startActivity(intent1);
+    }
+
 }
